9d2cc3bfcf0b907ca65791cc1bb3bc2bc0f8a281
Joanna Łukaszek <joanna.lukaszek@euvic.pl>
2018-02-28 10:27:17 +0100

    First commit

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = 'b454ccc14c929e7f843cbbd9bad7771f';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  //TODO: Make country code non static
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
